f286531ce9fe3fcde0e611d02794893d0f335358
auchmargust <fengjingya320@gmail.com>
2025-03-19 10:53:51 -0700

    Updated front-end production deployment. Front end will now be deployed from ./client/dist instead of ./public

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ import { fileURLToPath } from "url";
 import path from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -47,7 +47,7 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 // NOT FOUND MIDDLEWARE
 // used when a request is made to a route that does not exist.
